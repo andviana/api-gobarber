25bb7b45c84e0c42fcf7062bcc2427b574c71438
Anderson Viana <andiana@hotmnai.com>
2020-08-30 00:56:22 -0300

    added: user password field encrypt  before persist in database

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository, Repository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -16,10 +17,12 @@ class CreateUserService {
     if (checkUserExists) {
       throw new Error('Email address already used.');
     }
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     await usersRepository.save(user);
     return user;
