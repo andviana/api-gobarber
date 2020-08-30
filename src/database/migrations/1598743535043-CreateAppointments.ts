50520a953cf8b362fb5cf2e0ea0324516a61aead
Anderson Viana <andiana@hotmnai.com>
2020-08-29 23:21:25 -0300

    changed:  id columns have been type changed from varchar to uuid

--- src/database/migrations/1598743535043-CreateAppointments.ts
+++ src/database/migrations/1598743535043-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1598743535043
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
