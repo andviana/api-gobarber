1e3a9a46a68032508eb6e6ac23b5da52c8d3ff9b
Anderson Viana <andiana@hotmnai.com>
2020-09-24 23:33:04 -0300

    refactored: send mail with handlebars template file .hbs

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
@@ -27,6 +28,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail(
       {
         to: {
@@ -35,10 +43,10 @@ class SendForgotPasswordEmailService {
         },
         subject: '[GoBarber] Recuperação de senha',
         templateData: {
-          template: 'Olá, {{name}}: {{token}}',
+          file: forgotPasswordTemplate,
           variables: {
             name: user.name,
-            token,
+            link: `http://localhost:3000/reset_password?token=${token}`,
           },
         },
       },
