4c87831bf709653b74a5e3c51746aec0dd537b20
Anderson Viana <andiana@hotmnai.com>
2020-09-25 15:39:33 -0300

    implemented list of providers - service., repository, test, dto, routes and controller

---
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,13 @@
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+
+class ProvidersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const listProviders = container.resolve(ListProvidersService);
+    const providers = listProviders.execute({ user_id });
+    return response.json(providers);
+  }
+}
+export default ProvidersController;
