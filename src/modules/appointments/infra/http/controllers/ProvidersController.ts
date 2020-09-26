23475dd3eb458e9a8b8e3877a52c11ae7ac62910
Anderson Viana <andiana@hotmnai.com>
2020-09-25 21:58:36 -0300

    bugfix: added await clause on list provider service

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -6,7 +6,8 @@ class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const listProviders = container.resolve(ListProvidersService);
-    const providers = listProviders.execute({ user_id });
+    const providers = await listProviders.execute({ user_id });
+
     return response.json(providers);
   }
 }
